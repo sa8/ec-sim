c590897a5f31fc8c014b96b8134c38fd8e54c3fd
Sarah Azouvi <sarah8@bitbucket.org>
2020-02-06 20:08:13 +0000

    added the tipset case

--- tipset_leaderelection.py
+++ tipset_leaderelection.py
@@ -2,14 +2,13 @@ import networkx as nx
 import numpy as np 
 import itertools
 
-sim=100 #number of simulations
+sim=200 #number of simulations
 nh=67 #number of honest players
 na=33#number of adversarial players
 ntot=na+nh #total num,ber of players
 p=1./float(ntot) #proba for one leader to be elected
-Kmax=10 #length of the attack
-grind_max=4 #how many "grinds" we allow
-forks=[]
+Kmax=40 #length of the attack
+grind_max=30 #how many "grinds" we allow
 
 print("With Kmax = {k} and grind_max = {g}, we have: ".format(k=Kmax,g=grind_max))
 
@@ -19,20 +18,22 @@ print("With Kmax = {k} and grind_max = {g}, we have: ".format(k=Kmax,g=grind_max
 ### Grinding:
 def grind(n):
 	index_parent = n
-	lgth = G.node[n]['length']
+	wght = G.node[n]['weight']
 	c = G.node[n]['num_winner']
 	slot = G.node[n]['slot']
 	global ct
 	global current_list
 	if slot<Kmax:
 		for i in range(grind_max):
-			ca = np.random.binomial(na*c, p, 1)[0]
 			j=slot+i+1
-			if ca>0 and j<Kmax:
-				ct+=1
-				G.add_node(ct,slot=j,length=lgth+1,num_winner=ca,parent=n)
-				G.add_edge(index_parent,ct)
-				current_list.append(ct)#add all new nodes to the list
+			for k in range(c):
+				ca = np.random.binomial(na, p, 1)[0]
+				if ca>0 and j<Kmax:
+					ct+=1
+					G.add_node(ct,slot=j,weight=wght+k+1,num_winner=ca,parent=n)##add all the blocks
+					#I can include
+					G.add_edge(index_parent,ct)
+					current_list.append(ct)#add all new nodes to the list
 			#if no leader we need to go direct to the delay case 
 		#remove n from list
 	current_list.remove(n)
@@ -40,52 +41,51 @@ def grind(n):
 ##adversarial fork without grinding:
 forks=[]
 for i in range(sim):
-	nogrinding_fork_length=0
+	nogrinding_fork_weight=0
 	slot_number=0
 	ca = np.random.binomial(na, p, 1)[0]#honest players each toss a coin to see if elected leaders
 	while slot_number<Kmax:
-		if ca>0:#there were leaders elected, one block is created
-			nogrinding_fork_length+=1
+		nogrinding_fork_weight+=ca
 			#no leaders elected everyone moves to next slot
 		slot_number+=1
 		ca = np.random.binomial(na, p, 1)[0]#each honest leaders toss
 			#a new coin for that round
-	forks.append(nogrinding_fork_length)
+	forks.append(nogrinding_fork_weight)
+
+
+print "Weight of Fork without grinding (adversary): {f}.".format(f=np.average(forks))
+
 forks_adv=[]
-
-print "Length of Fork without grinding (adversary): {f}.".format(f=np.average(forks))
-
 for i in range(sim):
 	G=nx.DiGraph()
-	G.add_node(0,slot=0,length=0,num_winner=1)
+	G.add_node(0,slot=0,weight=0,num_winner=1)
 	ct=0
 	current_list=[0]
-	max_l=0
+	max_w=0
 	while current_list :
 		for n in current_list:
 			#print(current_list)
-			if G.node[n]['length']>max_l: max_l = G.node[n]['length']
+			if G.node[n]['weight']>max_w: max_w = G.node[n]['weight']
 			grind(n)
-
-	forks_adv.append(max_l)
-print "Length of adversarial fork with grinding: {f}".format(f=np.average(forks_adv))
+	forks_adv.append(max_w)
+print "Weight of adversarial fork with grinding: {f}".format(f=np.average(forks_adv))
 
 #what happens to the rest of the player?
 forks_honest=[]
 for i in range(sim):
-	honest_fork_length=0
+	honest_fork_weight=0
 	slot_number=0
 	ch = np.random.binomial(nh, p, 1)[0]
 	while slot_number<Kmax:
-		if ch>0:#there were leaders elected, one block is created
-			honest_fork_length+=1
+		#there were leaders elected, one block is created
+		honest_fork_weight+=ch
 			#no leaders elected everyone moves to next slot
 		slot_number+=1
 		ch = np.random.binomial(nh, p, 1)[0]#each honest leaders toss
 			#a new coin for that round
-	forks_honest.append(honest_fork_length)
+	forks_honest.append(honest_fork_weight)
 
-print "Length of fork for the rest of player: {f}.".format(f=np.average(forks_honest))
+print "Weight of fork for the rest of player: {f}.".format(f=np.average(forks_honest))
 
 
 quality=[1 if forks_adv[i]>=forks_honest[i] else 0 for i in range(sim)  ]
